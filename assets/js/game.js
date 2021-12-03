64a69f2f966456c41f15deeff27c6536efe9888c
Ragil14 <rahil.gilani@gmail.com>
2021-12-03 07:29:27 -0600

    added while Loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,68 +15,71 @@ var enemyAttack = 12;
 // "LOSE" - Player robot's health is zero or less
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round.
-  window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy-robot is alive
+  while(enemyHealth > 0) {
+            // Variable that gives player option to skip fight.
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-    // Variable that gives player option to skip fight.
-  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+            // if player chooses to fight, then fight.
+            if (promptFight === "fight" || promptFight === "FIGHT") {
+                    // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable.
+                enemyHealth = enemyHealth - playerAttack;
 
-    // if player chooses to fight, then fight.
-  if (promptFight === "fight" || promptFight === "FIGHT") {
-            // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable.
-        enemyHealth = enemyHealth - playerAttack;
+                    // Log a resulting message to the console so we know that it worked.
+                console.log(
+                    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+                );
 
-            // Log a resulting message to the console so we know that it worked.
-        console.log(
-            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-        );
+                    // check enemy's health w/(Conditional Statement aka Control Flow)
+                if (enemyHealth <= 0) {
+                    window.alert(enemyName + " has died!");
+                } 
+                else {
+                    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+                }
 
-            // check enemy's health w/(Conditional Statement aka Control Flow)
-        if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
-        } 
-        else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
-        }
+                    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+                playerHealth = playerHealth - enemyAttack;
 
-            // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-        playerHealth = playerHealth - enemyAttack;
+                    // Log a resulting message to the console so we know that it worked.
+                console.log(
+                    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+                );
 
-            // Log a resulting message to the console so we know that it worked.
-        console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-        );
+                    // check player's health
+                if (playerHealth <= 0) {
+                    window.alert(playerName + " has died!");
+                } 
+                else {
+                    window.alert(playerName + " still has " + playerHealth + " health left.");
+                }
+            } 
+                // if player chooses to skip
+            else if (promptFight === "skip" || promptFight === "SKIP") {
+                    // confirm player wants to skip
+                var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-            // check player's health
-        if (playerHealth <= 0) {
-            window.alert(playerName + " has died!");
-        } 
-        else {
-            window.alert(playerName + " still has " + playerHealth + " health left.");
-        }
-    } 
-    // if player chooses to skip
-  else if (promptFight === "skip" || promptFight === "SKIP") {
-        // confirm player wants to skip
-    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-        // if yes (true), leave fight
-    if (confirmSkip) {
-        window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 2;
+                    // if yes (true), leave fight
+                if (confirmSkip) {
+                    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                    // subtract money from playerMoney for skipping
+                    playerMoney = playerMoney - 2;
+                }
+                    // if no (false), ask question again by running fight() again
+                else {
+                    fight();
+                }
+                } 
+                else {
+                    window.alert("You need to choose a valid option. Try again!");
+                }
     }
-        // if no (false), ask question again by running fight() again
-    else {
-        fight();
-    }
-  } 
-  else {
-    window.alert("You need to choose a valid option. Try again!");
-  }
+
 };
 
-    // Call: 'function' w/in a 'for Loop' allowing player to fight all enemy robots.
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    // Call: function w/in a for Loop allowing player to fight all enemy robots.
+for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
