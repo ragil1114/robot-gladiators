243cc6033b6c1da9816e327242aac73aef159c9f
Ragil14 <rahil.gilani@gmail.com>
2021-12-08 22:53:18 -0600

    Blank/null response handling for the player-name prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -169,9 +169,22 @@ var shop = function() {
 };
 
 
+    // function to enter a valid name
+var getPlayerName = function() {
+    var name = "";
+  
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+  
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
+
     // Array (Custom Object)
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),  // Call for getPlayerName() function^^
     health: 100,
     attack: 10,
     money: 10,
